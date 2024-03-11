30209eda90ddf4a6142dc0a80c3bdc946b72419b
SapornaReizelFaith <saporna.reizelfaith@mscmarinduque.edu.ph>
2024-03-11 17:14:58 +0800

    song5

---
+++ song4.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fourth Song</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<?php
+echo"<div class='title'>";
+echo"<h1>Love Story</h1>";
+echo"<pre>";
+echo"\n[Intro]";
+echo"\nC  G  Am  F";
+ 
+echo"\n[Verse 1]";
+echo"\nC";
+echo"\nWe were both young when I first saw you";
+echo"\n  F";
+echo"\nI close my eyes and the flashback starts";
+echo"\n             Am                    F";
+echo"\nI'm standing there on a balcony in summer air";
+echo"\nC";
+echo"\nSee the lights, see the party, the ball gowns";
+echo"\nF";
+echo"\nSee you make your way through the crowd";
+echo"\n            Am";
+echo"\nAnd say, \"Hello\"";
+echo"\n           G";
+echo"\nLittle did I know";
+ 
+echo"\n[Pre-Chorus]";
+echo"\n     F                        G";
+echo"\nThat you were Romeo, you were throwing pebbles";
+echo"\n       Am                          C";
+echo"\nAnd my daddy said, \"Stay away from Juliet\"";
+echo"\n          F";
+echo"\n And I was crying on the staircase";
+echo"\n G                        Am  F      G";
+echo"\n Begging you please don't go,   and I said";
+
+echo"\n [Chorus]";
+echo"\n C";
+echo"\n Romeo take me somewhere we can be alone";
+echo"\n G";
+echo"\n I'll be waiting all there's left to do is run";
+echo"\n Am";
+echo"\n You'll be the prince and I'll be the princess";
+echo"\n F                 G                   C";
+echo"\n It's a love story baby just say \"yes\"";
+ 
+echo"\n[Verse 2]";
+echo"\n   C";
+echo"\nSo I sneak out to the garden to see you";
+echo"\nF";
+echo"\nWe keep quiet 'cause we're dead if they knew";
+echo"\n              Am";
+echo"\nSo close your eyes";
+echo"\n                       G";
+echo"\nEscape this town for a little while";
+
+echo"\n[Pre-Chorus]";
+echo"\n       F                       G";
+echo"\n'Cause you were Romeo, I was a scarlet letter";
+echo"\n       Am                        C";
+echo"\nAnd my daddy said stay away from Juliet";
+echo"\n            F";
+echo"\nBut you were everything to me I was";
+echo"\nG                        Am  F      G";
+echo"\nBegging you please don't go,   and I said";
+ 
+echo"\n[Chorus]";
+echo"\nC";
+echo"\nRomeo take me somewhere we can be alone";
+echo"\nG";
+echo"\nI'll be waiting all there's left to do is run";
+echo"\nYou'll be the prince and I'll be the princess";
+echo"\nF                 G";
+echo"\nIt's a love story baby just say yes";
+echo"\nC";
+echo"\nRomeo, save me, they try to tell me how to feel";
+echo"\nG";
+echo"\nThis love is difficult, but it's real,";
+echo"\nAm";
+echo"\nDon't be afraid, we'll make it out of this mess";
+echo"\nF                 G";
+echo"\nIt's a love story baby just say yes, oh";
+ 
+echo"\n[Solo]";
+echo"\nC  G  Am  F G";
+ 
+echo"\n[Bridge]";
+echo"\n               Am       F              C                          G";
+echo"\nI got tired of waiting,   wondering if you were ever coming around";
+echo"\n                   Am     F";
+echo"\nMy faith in you was fading";
+echo"\n       C                           G";
+echo"\nWhen I met you on the outskirts of town and I said";
+ 
+echo"\n[Chorus]";
+echo"\nC";
+echo"\nRomeo save me I've been feeling so alone";
+echo"\nG";
+echo"\nI keep waiting for you but you never come";
+echo"\n   Am";
+echo"\nIs this in my head, I don't know what to think";
+echo"\n   F                       G";
+echo"\nHe knelt to the ground and pulled out a ring and said";
+echo"\nD";
+echo"\nMarry me, Juliet you'll never have to be alone";
+echo"\nA";
+echo"\nI love you, and that's all I really know";
+echo"\n  Bm";
+echo"\nI talked to your dad, go pick out a white dress";
+echo"\nG                 A            D";
+echo"\nIt's a love story baby just say yes";
+ 
+echo"\n[Outro]";
+echo"\n   A      Bm";
+echo"\nUh oh, uh oh";
+echo"\n      G                                    D";
+echo"\nCause we were both young when I first saw you";
+
+echo"</div>"
+	?>
+
+	</body>
+	</html>
