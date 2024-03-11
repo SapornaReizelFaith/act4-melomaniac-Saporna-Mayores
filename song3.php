dc5a5d02f0417dc268f45699486f601de449b5e9
SapornaReizelFaith <saporna.reizelfaith@mscmarinduque.edu.ph>
2024-03-11 17:13:28 +0800

    song4

---
+++ song3.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Third Song</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<?php
+	
+echo"<div class='title'>";
+echo"<h1>Breathless</h1>";
+echo"<pre>";
+echo"\n[Intro]";
+echo"\nN.C.";
+echo"\nGo on, go on, leave me breathless";
+echo"\nA";
+echo"\n(Come on)";
+echo"\nA - E - Bm - F#m - E - A";
+ 
+echo"\nVerse 1:";
+echo"\nA              E";
+echo"\nThe daylight's fading slowly";
+echo"\nBm           F#m      E        A";
+echo"\nBut time with you is standing still";
+echo"\n            E";
+echo"\nBm            F#m           E       D";
+echo"\nThe slightest touch  and   I  feel weak";
+echo"\nE      F#m";
+echo"\nI cannot lie,";
+echo"\n     D     E      A   Asus4 - A";
+echo"\nFrom you I cannot hide";
+echo"\n        D          E       F#m";
+echo"\nAnd I'm losing the will to try";
+echo"\n      D              E";
+echo"\nCan't hide it, can't fight it";
+ 
+echo"\nChorus:";
+echo"\n   D         G                 A        Asus4  - A";
+echo"\nSo go on,    go on,   come on, leave me breath - less";
+echo"\nD                G          A        Asus4 - A";
+echo"\nTempt me,   tease me, until I can't de - ny   this";
+echo"\nD              G             A        Asus4 - A";
+echo"\nLov  -  ing feeling, make me long for your   kiss";
+echo"\nD           G                  (A)";
+echo"\nGo on,      go on,       ye  -  ah         Come On!";
+ 
+echo"\nA - E - Bm - F#m - E - A";
+ 
+echo"\nVerse 2:";
+echo"\nA              E";
+echo"\nAnd if there's no tomorrow";
+echo"\nBm         F#m          E        A";
+echo"\nAnd all we have  is    here and now";
+echo"\n          E";
+echo"\nI'm happy just to have you now";
+echo"\nBm             F#m     E                D";
+echo"\nYou're all the love   I    need some - how";
+echo"\nE      F#m";
+echo"\nIt's like a dream";
+echo"\n     D          E       A    Asus4 - A";
+echo"\nAl - though I'm not a - sleep";
+echo"\n      D     E       F#m";
+echo"\nAnd I never want to wake up";
+echo"\n      D              E";
+echo"\nDon't lose it, don't leave it";
+
+echo"\nChorus:";
+echo"\n   D         G                 A        Asus4  - A";
+echo"\nSo go on,    go on,   come on, leave me breath - less";
+echo"\nD                G          A        Asus4 - A";
+echo"\nTempt me,   tease me, until I can't de - ny   this";
+echo"\nD              G             A        Asus4 - A";
+echo"\nLov  -  ing feeling, make me long for your   kiss";
+echo"\nD           G                  (A)";
+echo"\nGo on,      go on,       ye  -  ah         Come On!";
+ 
+echo"\nA";
+echo"\nA - E - Bm - F#m - E - A  x2";
+ 
+echo"\nD     E      F#m";
+echo"\n  I'm like a knife";
+echo"\n     D          E       A    Asus4 - A";
+echo"\nFrom you I cannot hide";
+echo"\n      D     E       F#m";
+echo"\nAnd I've lost my will to try";
+echo"\n      D              E";
+echo"\nCan't hide it, can't fight it";
+ 
+echo"\nChorus:";
+echo"\n   D         G                 A        Asus4  - A";
+echo"\nSo go on,    go on,   come on, leave me breath - less";
+echo"\nD                G          A        Asus4 - A";
+echo"\nTempt me,   tease me, until I can't de - ny   this";
+echo"\nD              G             A        Asus4 - A";
+echo"\nLov  -  ing feeling, make me long for your   kiss";
+echo"\nD           G                    A        Asus4  - A";
+echo"\nGo on,      go on,       leave me breath - less";
+echo"\nD           G                    A        Asus4  - A";
+echo"\nGo on,      go on,       leave me breath - less";
+echo"\nD           G                    A        Asus4  - A";
+echo"\nGo on,      go on,       leave me breath - less";
+echo"\nD           D";
+echo"\nGo on,      go on";
+
+echo"</div>"
+	?>
+
+	</body>
+	</html>
