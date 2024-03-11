f9dab4f0354b5c7dceb0fbfbaa985f3a113a6eca
SapornaReizelFaith <saporna.reizelfaith@mscmarinduque.edu.ph>
2024-03-11 17:09:41 +0800

    thanks

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Melomaniac</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+			background-color: #F3BABA;; 
+			color: #333;
+		}
+
+		.title {
+			max-width: 800px;
+			margin: 20px auto;
+			padding: 20px;
+			background-color: #fff;
+			border-radius: 10px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+
+		.btn {
+			background-color: #000000;
+			color: #fff;
+			border: none;
+			padding: -1px 16px;
+			border-radius: 5px;
+			text-decoration: none;
+			font-weight: bold;
+			cursor: pointer;
+			margin-bottom: 20px;
+		}
+	</style>
+</head>
+<body>
+
+<div class="title">
+	<h1>FC Playlist</h1>
+
+	<a href="song1.php" class="btn">Traitor by Olivia Rodrigo</a><br>
+	<a href="song2.php" class="btn">Yellow by Coldplay</a><br>
+	<a href="song3.php" class="btn">Breathless by The Corrs</a><br>
+	<a href="song4.php" class="btn">Love Story by Taylor Swift</a><br>
+	<a href="song5.php" class="btn">Always Remember Us This Way by Lady Gaga</a><br>
+</div>
+
+</body>
+</html>
