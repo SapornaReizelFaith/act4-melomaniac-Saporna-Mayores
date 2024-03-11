add7c3f9d1b69b934e1c1d934a4a6052a4ff47f0
SapornaReizelFaith <saporna.reizelfaith@mscmarinduque.edu.ph>
2024-03-11 17:12:09 +0800

    song2

---
+++ song1.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Song</title>
+	<link rel="stylesheet" href="style.css">
+    </head>
+	<body>
+
+	<?php
+echo"<div class='title'>";
+echo"<h1>Traitor</h1>";
+echo"<pre>";
+echo"\n[Verse 1]";
+echo"\n C                      Em";
+echo"\nBrown guilty eyes and little white lies";
+echo"\n         Am                      Fmaj7";
+echo"\nYeah, I played dumb but I always knew";
+echo"\n      C                     Em";
+echo"\nThat you talk to her, maybe did even worse";
+echo"\n        Am                   Fmaj7";
+echo"\nI kept quiet so I could keep you";
+ 
+echo"\n[Pre-Chorus]";
+echo"\n               C";
+echo"\nAnd ain't it funny";
+echo"\n        Em";
+echo"\nHow you ran to her";
+echo"\n      Am                     Fmaj7";
+echo"\nThe second that we called it quits";
+echo"\n               C";
+echo"\nAnd ain't it funny";
+echo"\n         Em";
+echo"\nHow you said you were friends";
+echo"\n        Am                     Fmaj7";
+echo"\nNow it sure as hell don't look like it";
+
+echo"\n[Chorus]";
+echo"\n      C";
+echo"\nYou betrayed me";
+echo"\n   Em                              Am";
+echo"\nAnd I know that you'll never feel sorry";
+echo"\n   Fmaj7";
+echo"\nFor the way I hurt, yeah";
+echo"\n C";
+echo"\nYou talked to her";
+echo"\n     Em";
+echo"\nWhen we were together";
+echo"\n      Am";
+echo"\nLoved you at your worst";
+echo"\n    Fmaj7";
+echo"\nBut that didn't matter";
+echo"\n    C";
+echo"\nIt took you two weeks";
+echo"\n   Em";
+echo"\nTo go off and date her";
+echo"\n      Am";
+echo"\nGuess you didn't cheat";
+echo"\n    N.C.";
+echo"\nBut you're still a traitor";
+ 
+echo"\n[Verse 2]";
+echo"\nCv";
+echo"\nNow you bring her around";
+echo"\n     Em";
+echo"\nJust to shut me down";
+echo"\n         Am                   Fmaj7";
+echo"\nShow her off like she's a new trophy";
+echo"\n           C";
+echo"\nAnd I know if you were true";
+echo"\n            Em";
+echo"\nThere's no damn way that you";
+echo"\n               Am                      Fmaj7";
+echo"\nCould fall in love with somebody that quickly";
+ 
+echo"\n[Pre-Chorus]";
+echo"\n          C";
+echo"\nAin't it funny";
+echo"\n    Em";
+echo"\nAll the twisted games";
+echo"\n          Am                   Fmaj7";
+echo"\nAll the questions you used to avoid?";
+echo"\n           C";
+echo"\nAin't it funny?";
+echo"\n         Em";
+echo"\nRemember I brought her up";
+echo"\n         Am            Fmaj7";
+echo"\nAnd you told me I was paranoid";
+ 
+echo"\n[Chorus]";
+echo"\n      C";
+echo"\nYou betrayed me";
+echo"\n    Em                             Am";
+echo"\nAnd I know that you'll never feel sorry";
+echo"\n   Fmaj7";
+echo"\nFor the way I hurt, yeah";
+echo"\n C";
+echo"\nYou talked to her";
+echo"\n     Em";
+echo"\nWhen we were together";
+echo"\n      Am";
+echo"\nLoved you at your worst";
+echo"\n    Fmaj7";
+echo"\nBut that didn't matter";
+echo"\n    C";
+echo"\nIt took you two weeks";
+echo"\n   Em";
+echo"\nTo go off and date her";
+echo"\n       Am";
+echo"\nGuess you didn't cheat";
+echo"\nN.C.";
+echo"\nBut you're still a traitor";
+ 
+echo"\n[Bridge]";
+echo"\nC     Em          Am";
+echo"\n (Ah, ah, ah, ah) God, I wish that you had thought this through";
+echo"\nFmaj7";
+echo"\nBefore I went and fell in love with you";
+echo"\nC     Em                        Am";
+echo"\n (Ah, ah, ah, ah)  When she's sleeping in the bed we made";
+echo"\n          Fmaj7";
+echo"\nDon't you dare forget about the way";
+ 
+echo"\n[Chorus]";
+echo"\n      C";
+echo"\nYou betrayed me";
+echo"\n    Em                             Am";
+echo"\nCuz I know that you'll never feel sorry";
+echo"\n   Fmaj7";
+echo"\nFor the way I hurt, yeah";
+echo"\n C";
+echo"\nYou talked to her";
+echo"\n     Em";
+echo"\nWhen we were together";
+echo"\n     Am";
+echo"\nYou gave me your word";
+echo"\n    Fmaj7";
+echo"\nBut that didn't matter";
+echo"\n    C";
+echo"\nIt took you two weeks";
+echo"\n   Em";
+echo"\nTo go off and date her";
+echo"\n      Am";
+echo"\nGuess you didn't cheat";
+echo"\n    Fmaj7                        C  Em Am";
+echo"\nBut you're still, you're still a traitor (Ah, ah, ah, ah)";
+echo"\n      Fmaj7";
+echo"\nYeah, you're still a traitor";
+ 
+echo"\n[Outro]";
+echo"\nC    Em      Am";
+echo"\n Oh, oh, oh God, I wish that you had thought this through";
+echo"\n Fm";
+echo"\nbefore I went and fell in love with youecho";
+
+echo"</div>"
+ 	?>
+
+	</body>
+	</html>
