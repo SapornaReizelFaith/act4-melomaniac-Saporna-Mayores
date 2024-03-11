e2ba18b3e673b5495cd9d3e57fe716e3ace4bdd0
SapornaReizelFaith <saporna.reizelfaith@mscmarinduque.edu.ph>
2024-03-11 17:12:46 +0800

    song3

---
+++ song2.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Second Song</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<?php
+		
+echo"<div class='title'>";
+echo"<h1>Yellow</h1>";
+echo"<pre>";
+echo"\n[Verse 1]";
+echo"\nG";
+echo"\n  Look at the stars";
+echo"\n                       D";
+echo"\n Look how they shine for you";
+echo"\n                      Cmaj7";
+
+echo"\nAnd everything you do";
+echo"\nYeah, they were all yellow";
+echo"\nG";
+echo"\n  I came along";
+echo"\n                  D";
+echo"\nI wrote a song for you";
+echo"\n                         Cmaj7";
+echo"\nAnd all the things you do";
+ 
+echo"\nAnd it was called \"Yellow\"";
+echo"\nG                  D";
+echo"\n  So then I took my turn";
+echo"\n                             Cmaj7";
+echo"\nOh, what a thing to have done";
+echo"\n                      G  Gsus4";
+echo"\nAnd it was all yellow";
+ 
+echo"\n[Chorus 1]";
+echo"\nCmaj7";
+echo"\nYour skin";
+echo"\nEm7           D";
+echo"\nOh yeah, your skin and bones";
+echo"\nCmaj7  Em7          D";
+echo"\nTurn into something beautiful";
+echo"\nCmaj7     Em7        D          Cmaj7";
+echo"\nYou know, you know I love you so";
+ 
+echo"\nYou know I love you so";
+ 
+echo"\n[Instrumental]";
+echo"\nG    D    C    G  Gsus4";
+ 
+echo"\n[Verse 2]";
+echo"\nG";
+echo"\n  I swam across";
+echo"\n                   D";
+echo"\nI jumped across for you";
+echo"\n                      Cmaj7";
+echo"\nOh, what a thing to do";
+ 
+echo"\n'Cause you were all yellow";
+echo"\nG";
+echo"\n  I drew a line";
+echo"\n                 D";
+echo"\nI drew a line for you";
+echo"\n                      Cmaj7";
+echo"\nOh, what a thing to do";
+echo"\n                      G  Gsus4";
+echo"\nAnd it was all yellow";
+ 
+echo"\n[Chorus 2]";
+echo"\nC";
+echo"\nYour skin";
+echo"\nEm7           D";
+echo"\nOh yeah, your skin and bones";
+echo"\nCmaj7  Em7          D";
+echo"\nTurn into something beautiful";
+echo"\nCmaj7";
+echo"\nAnd you know";
+echo"\nEm7       D            Cmaj7";
+echo"\nFor you I bleed myself dry";
+ 
+echo"\nFor you I bleed myself dry";
+ 
+echo"\n[Instrumental]";
+echo"\nG    D    C    G  Gsus4";
+ 
+echo"\n[Bridge]";
+echo"\n     G                             D";
+echo"\nIt's true, look how they shine for you";
+echo"\n                        Cmaj7";
+echo"\nLook how they shine for you";
+echo"\n                        G";
+echo"\nLook how they shine for";
+echo"\n                        D";
+echo"\nLook how they shine for you";
+echo"\n                        Cmaj7";
+echo"\nLook how they shine for you";
+ 
+echo"\nLook how they shine";
+ 
+echo"\n[Outro]";
+echo"\nG";
+echo"\n  Look at the stars";
+echo"\n                       Dm7";
+echo"\nLook how they shine for you";
+echo"\n                          Cmaj7";
+echo"\nAnd all the things that you do";
+
+echo"</div>"
+	?>
+
+	</body>
+	</html>
